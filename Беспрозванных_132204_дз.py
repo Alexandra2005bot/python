4bb1977bf70f3e61555f179813bb4909e8ea91cc
Беспрозванных Александра <sashabesprozvannykh24@gmail.com>
2025-02-21 14:59:58 +0700

    Update Беспрозванных_132204_дз.py

--- Беспрозванных_132204_дз.py
+++ Беспрозванных_132204_дз.py
@@ -1,7 +1,7 @@
 import math
 a = float(input("Длина стороны а: "))
 b = float(input("Длина стороны b: "))
-angle_derees = float(input("Введите угол между сторонами в градусах: "))
+angle_degrees = float(input("Введите угол между сторонами в градусах: "))
 angle_radians =
 math.radians(angle_degrees)
 c = math.sqrt(a**2 + b**2 + - 2 * a * b *
