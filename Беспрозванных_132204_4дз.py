d143d41a750c4141634d04fbd6471bb1ff6ca47b
Беспрозванных Александра <sashabesprozvannykh24@gmail.com>
2025-03-26 16:56:06 +0700

    Update Беспрозванных_132204_4дз.py

--- Беспрозванных_132204_4дз.py
+++ Беспрозванных_132204_4дз.py
@@ -1,9 +1,9 @@
-def find_primes_no_return(n):
+def find_primes(n):
     
     try:
         n = int(n)
         if n <= 1:
-            print("Введите число больше 1")
+            print("Введите число больше 2")
         else:
             primes = []
             for num in range(2, n + 1):
@@ -22,4 +22,4 @@ def find_primes_no_return(n):
         print("Некорректный ввод, введите число")
 
 n = input("Введите число N: ")
-find_primes_no_return(n)
+find_primes(n)
